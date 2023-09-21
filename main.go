2373933a91af86d55a0d298b7a78a4afb046d6b8
Tristan Davis <Tristandavis888@gmail.com>
2023-09-20 21:28:35 -0700

    main.go with beginning of WS manager

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Server running on http://localhost:8080/")
+	setupAPI()
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	
+}
+
+func setupAPI() {
+	// Create a Manager instance used to handle WebSocket Connections
+	manager := NewManager()
+
+	// Serve the ./frontend directory at Route /
+	http.Handle("/", http.FileServer(http.Dir("./frontend")))
+	http.HandleFunc("/ws", manager.serveWS)
+}
